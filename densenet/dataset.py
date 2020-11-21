25809518b3e902266f09ae4194fa8e1671135fc2
chung <phungdaovinhchung1@gmail.com>
2020-11-22 03:27:21 +0700

    add dataset.py change preprocess.py

---
+++ densenet/dataset.py
@@ -0,0 +1,37 @@
+from os.path import join
+
+import cv2
+import numpy as np
+import pandas as pd
+import torch
+import torch.utils.data as data
+
+
+class dataloader(data.Dataset):
+    def __init__(self, txt_path, data_path, preproc=None):
+        self.preproc = preproc
+        self.imgs_path = []
+        self.labels = []
+        df = pd.read_csv(txt_path)
+        img_list = list(df['image'])
+        for i in img_list:
+            tmp_label = df[df['image']==i].apply(lambda x: df.columns[x==1], axis = 1).to_string(header=False)
+            self.labels.append(tmp_label.split()[1][8:-3])
+            self.imgs_path.append(join(data_path, i+'.jpg'))
+
+    def __len__(self):
+        return len(self.imgs_path)
+
+    def __getitem__(self, index):
+#         import pdb;pdb.set_trace()
+        
+        img = cv2.imread(self.imgs_path[index])
+
+        label = self.labels[index]
+        target = np.array(label)
+        if self.preproc is not None:
+#             print(self.flags[index])
+            img, target = self.preproc(img, target)
+
+        # return torch.from_numpy(img), target
+        return torch.unsqueeze(torch.from_numpy(img), 0), target 
--- densenet/preprocess.py
+++ densenet/preprocess.py
@@ -1,146 +1,251 @@
-import os
-import os.path
-
 import cv2
 import numpy as np
-import torch
-import torch.utils.data as data
-import pandas as pd 
+import random
+from utils.box_utils import matrix_iof
 
 
-class dataloader(data.Dataset):
-    def __init__(self, txt_path, preproc=None):
-        self.preproc = preproc
-        self.imgs_path = []
-        self.words = []
-        f = open(txt_path,'r')
-        lines = f.readlines()
-        isFirst = True
-        labels = []
-        flag_62 = False
-        self.flags = []
-        for line in lines:
-            line = line.rstrip()
-            if line.startswith('#'):
-                flag_62 = False
+def _crop(image, boxes, labels, landm, img_dim, scale_flag):
+    height, width, _ = image.shape
+    # height, width = image.shape
+    pad_image_flag = True
+    # print(scale_flag)
+    for _ in range(250):
+        """
+        if random.uniform(0, 1) <= 0.2:
+            scale = 1.0
+        else:
+            scale = random.uniform(0.3, 1.0)
+        """
+        PRE_SCALES = [0.3, 0.45, 0.6, 0.8, 1.0]
+        if scale_flag:
+            scale = 0.45
+        else:
+            scale = random.choice(PRE_SCALES)
+        short_side = min(width, height)
+        w = int(scale * short_side)
+        h = w
 
-                if line.startswith('# 62'):
-#                     import pdb; pdb.set_trace()
-                    flag_62 = False
-                    
-                if isFirst is True:
-                    isFirst = False
-                else:
-                    
-                    labels_copy = labels.copy()
-                    self.words.append(labels_copy)
-                    if flag_62:
-                        self.words.append(labels.copy())
-                        self.words.append(labels.copy())
-                        self.words.append(labels.copy())
-                        
-                    labels.clear()
-                path = line[2:]
-                path = txt_path.replace('label.txt','images/') + path
-                self.imgs_path.append(path)
-                if flag_62:
-                    self.flags.append(True)
-                    self.flags.append(True)
-                    self.flags.append(True)
-                    self.flags.append(True)
-                    self.imgs_path.append(path)
-                    self.imgs_path.append(path)
-                    self.imgs_path.append(path)
-                else:
-                    self.flags.append(False)
-            else:
-                line = line.split(' ')
-                label = [float(x) for x in line]
-                labels.append(label)
+        if width == w:
+            l = 0
+        else:
+            l = random.randrange(width - w)
 
-        self.words.append(labels)
-        if flag_62:
-            self.words.append(labels.copy())
-            self.words.append(labels.copy())
-            self.words.append(labels.copy())
+        if height == h:
+            t = 0
+        else:
+            t = random.randrange(height - h)
 
-    def __len__(self):
-        return len(self.imgs_path)
+        roi = np.array((l, t, l + w, t + h))
 
-    def __getitem__(self, index):
-#         import pdb;pdb.set_trace()
-        
-        img = cv2.imread(self.imgs_path[index])
-        try:
-            height, width, _ = img.shape
-        except:
-            print(self.imgs_path[index])
+        value = matrix_iof(boxes, roi[np.newaxis])
+        flag = (value >= 1)
+        if not flag.any():
+            continue
 
-        ############## downscale ############## 
-        img = cv2.resize(img, (int(width/2), int(height/2)))
-        height, width, _ = img.shape
-        #######################################
-        
-        labels = self.words[index]
-        annotations = np.zeros((0, 15))
-        if len(labels) == 0:
-            return annotations
-        for idx, label in enumerate(labels):
-            annotation = np.zeros((1, 15))
+        centers = (boxes[:, :2] + boxes[:, 2:]) / 2
+        mask_a = np.logical_and(roi[:2] < centers, centers < roi[2:]).all(axis=1)
+        boxes_t = boxes[mask_a].copy()
+        labels_t = labels[mask_a].copy()
+        landms_t = landm[mask_a].copy()
+        landms_t = landms_t.reshape([-1, 5, 2])
 
-            ############## downscale ############## 
-            annotation[0, 0] = int(label[0]/2) # x1
-            annotation[0, 1] = int(label[1]/2) # y1
-            annotation[0, 2] = int((label[0] + label[2])/2)  # x2
-            annotation[0, 3] = int((label[1] + label[3])/2)  # y2
+        if boxes_t.shape[0] == 0:
+            continue
 
-            # landmarks
-            annotation[0, 4] = label[4]/2    # l0_x
-            annotation[0, 5] = label[5]/2    # l0_y
-            annotation[0, 6] = label[7]/2    # l1_x
-            annotation[0, 7] = label[8]/2    # l1_y
-            annotation[0, 8] = label[10]/2   # l2_x
-            annotation[0, 9] = label[11]/2   # l2_y
-            annotation[0, 10] = label[13]/2  # l3_x
-            annotation[0, 11] = label[14]/2  # l3_y
-            annotation[0, 12] = label[16]/2  # l4_x
-            annotation[0, 13] = label[17]/2  # l4_y
-            #######################################
+        image_t = image[roi[1]:roi[3], roi[0]:roi[2]]
 
-            if (annotation[0, 4]<0):
-                annotation[0, 14] = -1
-            else:
-                annotation[0, 14] = 1
+        boxes_t[:, :2] = np.maximum(boxes_t[:, :2], roi[:2])
+        boxes_t[:, :2] -= roi[:2]
+        boxes_t[:, 2:] = np.minimum(boxes_t[:, 2:], roi[2:])
+        boxes_t[:, 2:] -= roi[:2]
 
-            annotations = np.append(annotations, annotation, axis=0)
-        target = np.array(annotations)
-        if self.preproc is not None:
-#             print(self.flags[index])
-            img, target = self.preproc(img, target, self.flags[index])
+        # landm
+        landms_t[:, :, :2] = landms_t[:, :, :2] - roi[:2]
+        landms_t[:, :, :2] = np.maximum(landms_t[:, :, :2], np.array([0, 0]))
+        landms_t[:, :, :2] = np.minimum(landms_t[:, :, :2], roi[2:] - roi[:2])
+        landms_t = landms_t.reshape([-1, 10])
 
-        # return torch.from_numpy(img), target
-        return torch.unsqueeze(torch.from_numpy(img), 0), target 
 
-def detection_collate(batch):
-    """Custom collate fn for dealing with batches of images that have a different
-    number of associated object annotations (bounding boxes).
+	# make sure that the cropped image contains at least one face > 16 pixel at training image scale
+        b_w_t = (boxes_t[:, 2] - boxes_t[:, 0] + 1) / w * img_dim
+        b_h_t = (boxes_t[:, 3] - boxes_t[:, 1] + 1) / h * img_dim
+        mask_b = np.minimum(b_w_t, b_h_t) > 0.0
+        boxes_t = boxes_t[mask_b]
+        labels_t = labels_t[mask_b]
+        landms_t = landms_t[mask_b]
 
-    Arguments:
-        batch: (tuple) A tuple of tensor images and lists of annotations
+        if boxes_t.shape[0] == 0:
+            continue
 
-    Return:
-        A tuple containing:
-            1) (tensor) batch of images stacked on their 0 dim
-            2) (list of tensors) annotations for a given image are stacked on 0 dim
-    """
-    targets = []
-    imgs = []
-    for _, sample in enumerate(batch):
-        for _, tup in enumerate(sample):
-            if torch.is_tensor(tup):
-                imgs.append(tup)
-            elif isinstance(tup, type(np.empty(0))):
-                annos = torch.from_numpy(tup).float()
-                targets.append(annos)
+        pad_image_flag = False
 
-    return (torch.stack(imgs, 0), targets)
+        return image_t, boxes_t, labels_t, landms_t, pad_image_flag
+    return image, boxes, labels, landm, pad_image_flag
+
+
+def _distort(image):
+
+    def _convert(image, alpha=1, beta=0):
+        tmp = image.astype(float) * alpha + beta
+        tmp[tmp < 0] = 0
+        tmp[tmp > 255] = 255
+        image[:] = tmp
+
+    image = image.copy()
+
+    # _convert(image, beta=random.uniform(-32, 32))
+    
+    if bool(random.getrandbits(1)):
+
+        #brightness distortion
+        if bool(random.getrandbits(1)):
+            _convert(image, beta=random.uniform(-32, 32))
+
+        #contrast distortion
+        if bool(random.getrandbits(1)):
+            _convert(image, alpha=random.uniform(0.5, 1.5))
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        #saturation distortion
+        if bool(random.getrandbits(1)):
+            _convert(image[:, :, 1], alpha=random.uniform(0.5, 1.5))
+
+        #hue distortion
+        if bool(random.getrandbits(1)):
+            tmp = image[:, :, 0].astype(int) + random.randint(-18, 18)
+            tmp %= 180
+            image[:, :, 0] = tmp
+
+        image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+
+    else:
+
+        #brightness distortion
+        if bool(random.getrandbits(1)):
+            _convert(image, beta=random.uniform(-32, 32))
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        #saturation distortion
+        if bool(random.getrandbits(1)):
+            _convert(image[:, :, 1], alpha=random.uniform(0.5, 1.5))
+
+        #hue distortion
+        if bool(random.getrandbits(1)):
+            tmp = image[:, :, 0].astype(int) + random.randint(-18, 18)
+            tmp %= 180
+            image[:, :, 0] = tmp
+
+        image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+
+        #contrast distortion
+        if bool(random.getrandbits(1)):
+            _convert(image, alpha=random.uniform(0.5, 1.5))
+    
+    return image
+
+
+def _expand(image, boxes, fill, p):
+    if random.randrange(2):
+        return image, boxes
+
+    height, width, depth = image.shape
+
+    scale = random.uniform(1, p)
+    w = int(scale * width)
+    h = int(scale * height)
+
+    left = random.randint(0, w - width)
+    top = random.randint(0, h - height)
+
+    boxes_t = boxes.copy()
+    boxes_t[:, :2] += (left, top)
+    boxes_t[:, 2:] += (left, top)
+    expand_image = np.empty(
+        (h, w, depth),
+        dtype=image.dtype)
+    expand_image[:, :] = fill
+    expand_image[top:top + height, left:left + width] = image
+    image = expand_image
+
+    return image, boxes_t
+
+
+def _mirror(image, boxes, landms):
+    _, width, _ = image.shape
+    if random.randrange(2):
+        image = image[:, ::-1]
+        boxes = boxes.copy()
+        boxes[:, 0::2] = width - boxes[:, 2::-2]
+
+        # landm
+        landms = landms.copy()
+        landms = landms.reshape([-1, 5, 2])
+        landms[:, :, 0] = width - landms[:, :, 0]
+        tmp = landms[:, 1, :].copy()
+        landms[:, 1, :] = landms[:, 0, :]
+        landms[:, 0, :] = tmp
+        tmp1 = landms[:, 4, :].copy()
+        landms[:, 4, :] = landms[:, 3, :]
+        landms[:, 3, :] = tmp1
+        landms = landms.reshape([-1, 10])
+
+    return image, boxes, landms
+
+
+def _pad_to_square(image, rgb_mean, pad_image_flag):
+    if not pad_image_flag:
+        return image
+    height, width, _ = image.shape
+    # height, width = image.shape
+    long_side = max(width, height)
+    image_t = np.empty((long_side, long_side, 3), dtype=image.dtype)
+    # image_t = np.empty((long_side, long_side), dtype=image.dtype)
+    image_t[:, :] = rgb_mean
+    # image_t[:, :] = 117
+    image_t[0:0 + height, 0:0 + width] = image
+    return image_t
+
+
+def _resize_subtract_mean(image, insize, rgb_mean):
+    interp_methods = [cv2.INTER_LINEAR, cv2.INTER_CUBIC, cv2.INTER_AREA, cv2.INTER_NEAREST, cv2.INTER_LANCZOS4]
+    interp_method = interp_methods[random.randrange(5)]
+    image = cv2.resize(image, (insize, insize), interpolation=interp_method)
+    # image = image.astype(np.float32)
+    # image -= rgb_mean
+    # image -= 117
+    # return image.transpose(2, 0, 1)
+    return image
+
+
+class preproc(object):
+
+    def __init__(self, img_dim, rgb_means):
+        self.img_dim = img_dim
+        self.rgb_means = rgb_means
+
+    def __call__(self, image, targets, flag):
+        assert targets.shape[0] > 0, "this image does not have gt"
+
+        boxes = targets[:, :4].copy()
+        labels = targets[:, -1].copy()
+        landm = targets[:, 4:-1].copy()
+#         print(labels.shape)
+        image_t, boxes_t, labels_t, landm_t, pad_image_flag = _crop(image, boxes, labels, landm, self.img_dim, flag)
+        image_t = _distort(image_t)
+        image_t = _pad_to_square(image_t, self.rgb_means, pad_image_flag)
+        image_t, boxes_t, landm_t = _mirror(image_t, boxes_t, landm_t)
+        height, width, _ = image_t.shape
+        image_t = _resize_subtract_mean(image_t, self.img_dim, self.rgb_means)
+
+        boxes_t[:, 0::2] /= width
+        boxes_t[:, 1::2] /= height
+
+        landm_t[:, 0::2] /= width
+        landm_t[:, 1::2] /= height
+
+        labels_t = np.expand_dims(labels_t, 1)
+        targets_t = np.hstack((boxes_t, landm_t, labels_t))
+
+        return image_t, targets_t
